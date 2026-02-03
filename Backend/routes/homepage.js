c24b97e91134294f6e69a3b004b29ded6482c170
Kieme440 <Kieme440@gmail.com>
2026-02-03 09:44:05 +0100

    fixes

--- Backend/routes/homepage.js
+++ Backend/routes/homepage.js
@@ -3,5 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", getHomepage);
+router.get("/", getOurServices);
+router.get("/", getAboutUs);
 
 export default router;
